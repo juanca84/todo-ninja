799a44f5994f19914bb708c4d8256886b23174ab
Juan Carlos Condori Machicado <jcondori@agetic.gob.bo>
2019-03-18 20:01:39 -0400

    Tutorial todo-ninja nro.20

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="dashboard">
+    <h1 class="subheading grey--text">Dashboard</h1>
+    <v-container class="my-5">
+      <v-layout row class="mb-3">
+        <v-tooltip top>
+          <v-btn small flat color="grey" @click="sortBy('title')" slot="activator">
+            <v-icon left small>folder</v-icon>
+            <span class="cation text-lowercase">By project name</span>
+          </v-btn>
+          <span>Sort projects by project name</span>
+        </v-tooltip>
+        <v-tooltip top>
+          <v-btn small flat color="grey" @click="sortBy('person')" slot="activator">
+            <v-icon left small>person</v-icon>
+            <span class="cation text-lowercase">By person</span>
+          </v-btn>
+          <span>Sort projects by person</span>
+        </v-tooltip>
+      </v-layout>
+      <v-card flat v-for="project in projects" :key="project.title">
+        <v-layout row wrap :class="`pa-3 project ${project.status}`">
+          <v-flex xs12 md6>
+            <div class="caption grey--text">Project Title</div>
+            <div>{{ project.title }}</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Person</div>
+            <div>{{ project.person }}</div>
+          </v-flex>
+          <v-flex xs6 sm4 md2>
+            <div class="caption grey--text">Due by</div>
+            <div>{{ project.due }}</div>
+          </v-flex>
+          <v-flex xs2 sm4 md2>
+              <div>
+                <v-chip small :class="`${project.status} white--text caption my-2`">{{project.status}}</v-chip>
+              </div>
+
+          </v-flex>
+        </v-layout>
+        <v-divider></v-divider>
+      </v-card>
+      <!-- <v-layout row wrap>
+        <v-flex xs12 md6>
+          <v-btn outline block class='primary'>1</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class='primary'>2</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class='primary'>2</v-btn>
+        </v-flex>
+        <v-flex xs4 md2>
+          <v-btn outline block class='primary'>2</v-btn>
+        </v-flex>
+      </v-layout>
+      <v-layout row wrap justify-space-between>
+        <v-flex xs4 md3>
+          <v-btn outline block class='success'>1</v-btn>
+        </v-flex>
+        <v-flex xs4 md3>
+          <v-btn outline block class='success'>1</v-btn>
+        </v-flex>
+      </v-layout> -->
+    </v-container>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        projects: [
+          { title: 'Design a new website', person: 'The Net Ninja', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+          { title: 'Management a Database', person: 'Chun Ly', due: '1st Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+          { title: 'Design a new sistem', person: 'La Pantera Rosa', due: '1st Jan 2019', status: 'complete', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+          { title: 'Create a inteligense artificial', person: 'Caifanes', due: '1st Jan 2019', status: 'overdue', content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'},
+        ]
+      }
+    },
+    methods: {
+      sortBy(prop) {
+        this.projects.sort((a,b)=> a[prop]< b[prop] ? -1 : 1)
+      }
+    }
+  }
+</script>
+
+<style>
+  .project.complete {
+    border-left: 4px solid #3cd1c2;
+  }
+  .project.ongoing {
+    border-left: 4px solid orange;
+  }
+  .project.overdue {
+    border-left: 4px solid tomato;
+  }
+  .v-chip.complete {
+    background: #3cd1c2;
+  }
+  .v-chip.ongoing {
+    background: #ffaa2c;
+  }
+  .v-chip.overdue {
+    background: #f83e70;
+  }
+</style>
